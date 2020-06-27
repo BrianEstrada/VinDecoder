d76b111e3213190ee3cbca8ed91e2cb623995b2f
Brian Estrada <lovesguitar@gmail.com>
2020-06-27 11:02:36 +0200

    Added check for vin length

--- src/main/kotlin/com/brianestrada/vindecoder/VinDecoder.kt
+++ src/main/kotlin/com/brianestrada/vindecoder/VinDecoder.kt
@@ -1,6 +1,13 @@
 package com.brianestrada.vindecoder
 
 class VinDecoder(vin: String) {
+
+    init {
+        if (vin.length != 17) {
+            throw RuntimeException("Invalid vin length ${vin.length}")
+        }
+    }
+
     private val vinCountryString: String = vin.substring(0, 2)
     private val vinMakerString: String = vin.substring(0, 3)
 
